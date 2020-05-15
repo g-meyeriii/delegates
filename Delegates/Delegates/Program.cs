4d076147c967b3ba0a8fd730d2478d4be9b5ba87
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-15 08:23:06 -0700

    anonymous methods

--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -4,35 +4,55 @@ namespace Delegates
 {
     class Program
     {
-        public delegate double PerformCalculation(double x, double y);
+        //public delegate double PerformCalculation(double x, double y);
 
-        public static double Addition(double a, double b)
-        {
-            Console.WriteLine("a+b is :   " + (a+b));
-            return a + b;
-        }
-        public static double Division(double a, double b)
-        {
-            Console.WriteLine("a/b is :   " + (a / b));
-            return a / b;
-        }
-        public static double Subtraction(double a, double b)
-        {
-            Console.WriteLine("a-b is :   " + (a - b));
-            return a - b;
-        }
+        //public static double Addition(double a, double b)
+        //{
+        //    Console.WriteLine("a+b is :   " + (a+b));
+        //    return a + b;
+        //}
+        //public static double Division(double a, double b)
+        //{
+        //    Console.WriteLine("a/b is :   " + (a / b));
+        //    return a / b;
+        //}
+        //public static double Subtraction(double a, double b)
+        //{
+        //    Console.WriteLine("a-b is :   " + (a - b));
+        //    return a - b;
+        //}
+        public delegate string GetTextDelegate(string name);
+
         static void Main(string[] args)
         {
-            PerformCalculation getSum = Addition;
-            //getSum(5.0, 5.0);
-            PerformCalculation getQuotient = Division;
-            //getQuotient(5.0, 5.0);
+            // Creating an anonymous method or inline delegate
+            GetTextDelegate getTextDelegate = delegate (string name)
+            {
+                return "Hello, " + name;
+            };
+            Console.WriteLine(getTextDelegate("Sophie"));
+            Display(getTextDelegate);
 
-            PerformCalculation multiCalc = getSum + getQuotient;
-            multiCalc += Subtraction;
-            multiCalc -= getSum;
-            multiCalc(3.2, 3.2);
+            //PerformCalculation getSum = Addition;
+            ////getSum(5.0, 5.0);
+            //PerformCalculation getQuotient = Division;
+            ////getQuotient(5.0, 5.0);
 
+            //PerformCalculation multiCalc = getSum + getQuotient;
+            //multiCalc += Subtraction;
+            //multiCalc -= getSum;
+            //multiCalc(3.2, 3.2);
+
+
+
+        }
+        static void Display(GetTextDelegate getTextDelegate)
+        {
+            Console.WriteLine(getTextDelegate("World"));
+        }
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello");
         }
     }
 }
